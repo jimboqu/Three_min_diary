f7ce00bc84b89e03d4c2fae2cdaf6e6ceadf3513
jimboqu <jamesm.quilter@gmail.com>
2018-07-04 17:33:44 +0100

    Added alerts, styling, static

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,9 @@
 class Day < ApplicationRecord
   belongs_to :user
+  validates :gift, presence: true, length: {minimum: 20}
+  validates :thing, presence: true, length: {minimum: 20}
+  validates :thing, presence: true, length: {minimum: 20}
+  validates :daily, presence: true, length: {minimum: 20}
+  validates :effort, presence: true, length: {minimum: 20}
+  validates :improvement, presence: true, length: {minimum: 20}
 end
