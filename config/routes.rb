0471168ec88ee0264b70fc9b933e5d3ac2dfb65b
jimboqu <jamesm.quilter@gmail.com>
2018-07-04 14:12:38 +0100

    Added Bulma to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :days
   root 'days#index'
+  get '/about', to: "static#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
